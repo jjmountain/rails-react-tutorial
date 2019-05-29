f6fc322d72c75ac039ca35cba593b8441c8ca119
James Devereux <jjmountain@Apples-MacBook-Pro.local>
2019-05-29 19:58:38 +0900

    basic router working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
   	get 'things', to: 'things#index'
   end
+  get '*path', to: 'static#index', constraints: ->(req) do
+  	!req.xhr? && req.format.html?
+  end
+  root 'static#index'
 end
